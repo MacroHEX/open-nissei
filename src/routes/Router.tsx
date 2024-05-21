168fb8bdc44e55c7daadb1cd77ab690e92cd3ffc
MacroHEX <martin18py@gmail.com>
2024-05-20 21:47:31 -0400

    feat: Agregado Route para Registrarse

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,6 +1,7 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {Home} from "@/screens/Home.tsx";
 import {Login} from "@/screens/Login.tsx";
+import {Signup} from "@/screens/Signup.tsx";
 
 export const RouterApp = () => {
   return (
@@ -8,6 +9,7 @@ export const RouterApp = () => {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<Signup/>}/>
       </Routes>
     </BrowserRouter>
   );
